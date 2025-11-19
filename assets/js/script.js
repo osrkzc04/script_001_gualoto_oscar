94520988d81e643e193f07515bc120345297e941
Oscar Gualoto <opgualoto@puce.edu.ec>
2025-11-18 20:03:01 -0500

    feat: add tailwind

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,23 @@ function createMatrix(dimension,container,divisorFizz,divisorBuzz){
     let counter =1 
     for(i=0;i<dimension;i++){
         const div = document.createElement("div")
-        addClass(div,"demo")
-        
+        addClass(div,"flex")
+        addClass(div,"flex-row")
+        addClass(div,"space-x-4")
+        addClass(div,"leading-6")
+        addClass(div,"font-bold")
+        addClass(div,"mb-4")
+
         for(j=0;j<dimension;j++){
             const content = document.createElement("div")
-            addClass(content,"content")
+            addClass(content,"flex")
+            addClass(content,"h-30")
+            addClass(content,"w-30")
+            addClass(content,"basis-64")
+            addClass(content,"items-center")
+            addClass(content,"justify-center")
+            addClass(content,"rounded-lg")
+            
             checkFizzBuzz(content,counter,divisorFizz,divisorBuzz)
             content.innerHTML=counter
             div.appendChild(content)
@@ -25,19 +37,30 @@ function checkFizzBuzz(square, number, divisorFizz, divisorBuzz) {
   const isBuzz = number % divisorBuzz === 0
 
   if (isFizz && isBuzz) {
-    addClass(square, "fizzBuzz")
-    addClass(square, "fizz")
-    addClass(square, "buzz")
+    addClass(square, "bg-cyan-500")
+    addClass(square, "hover:bg-cyan-800")
+    addClass(square, "text-white")
   } else if (isFizz) {
-    addClass(square, "fizz")
+
+    addClass(square, "bg-indigo-500")
+    addClass(square, "hover:bg-indigo-800")
+    addClass(square, "text-white")
+
   } else if (isBuzz) {
-    addClass(square, "buzz")
+    addClass(square, "bg-pink-500")
+    addClass(square, "hover:bg-pink-800")
+
+    addClass(square, "text-white")
+  }
+  else {
+    addClass(square,"bg-slate-200")
+    addClass(square,"hover:bg-slate-300")
   }
 }
 
 
 function btnGenerateOnClick(){
-  const container = document.querySelector(".container")
+  const container = document.querySelector("#container")
   const inputDimension = document.getElementById("size").value
   const inputFizz = document.getElementById("fizz-divisor").value
   const inputBuzz = document.getElementById("buzz-divisor").value
